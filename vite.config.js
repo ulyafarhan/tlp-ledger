c969159a78f8135957a3eacac7c6fb12b88b28da
Muhammad Ulya Farhan <muhammadulyafarhan@gmail.com>
2025-12-06 16:03:54 +0700

    update max size program

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
-        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024
+        maximumFileSizeToCacheInBytes: 30 * 1024 * 1024
       }
     })
   ],
@@ -38,4 +38,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 5000, 
+  }
 })
